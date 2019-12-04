05eac7304417688fc00673aa2de5dfc002bd48ed
Danielauler <daniel-auler@hotmail.com>
2019-12-04 12:34:01 -0300

    código refeito

--- code/projetoFinal/compile_n_run.sh
+++ code/projetoFinal/compile_n_run.sh
@@ -2,6 +2,8 @@
 
 echo ">>> Criando pasta para guardar executável..."
 mkdir build
+echo ">>> Copiando imagem de teste para a pasta"
+mv foto_img.jpg build
 cd build
 echo ">>> Preparando arquivos para compilação..."
 cmake ../
